0f79342669f1f70d5166bcd846908a7c3e2ec09d
Ivan Zykov <ivan.v.zykov@gmail.com>
2026-02-19 09:58:31 +0100

    Improve Consecutive strings

--- src/main/java/net/ivanvzykov/codewarsjava/LongestConsec.java
+++ src/main/java/net/ivanvzykov/codewarsjava/LongestConsec.java
@@ -1,31 +1,29 @@
 package net.ivanvzykov.codewarsjava;
 
-import java.util.Arrays;
-
 // https://www.codewars.com/kata/56a5d994ac971f1ac500003e
 class LongestConsec {
 
     public static String longestConsec(String[] strarr, int k) {
         if (strarr.length < k || k <= 0) return "";
 
-        var lengthsOfStrings = Arrays.stream(strarr)
-                .map(String::length)
-                .toList();
+        var bestStartIdx = 0;
+        var curLen = 0;
+        for (int i = 0; i < k; i++) {
+            curLen += strarr[i].length();
+        }
+        var maxLen = curLen;
 
-        var firstIdxOfLongestConsStrings = 0;
-        var maxLenOfConsStrings = 0;
-        for (int i = 0; i + k <= lengthsOfStrings.size(); i++) {
-            var sublist = lengthsOfStrings.subList(i, i + k);
-            var lenOfConsStrings = sublist.stream().reduce(Integer::sum)
-                    .orElseThrow(IllegalArgumentException::new);
-            if (maxLenOfConsStrings < lenOfConsStrings) {
-                maxLenOfConsStrings = lenOfConsStrings;
-                firstIdxOfLongestConsStrings = i;
+        for (int i = 1; i + k <= strarr.length; i++) {
+            curLen += strarr[i + k - 1].length();
+            curLen -= strarr[i - 1].length();
+            if (maxLen < curLen) {
+                maxLen = curLen;
+                bestStartIdx = i;
             }
         }
 
         var builder = new StringBuilder();
-        for (int i = firstIdxOfLongestConsStrings; i < firstIdxOfLongestConsStrings + k; i++) {
+        for (int i = bestStartIdx; i < bestStartIdx + k; i++) {
             builder.append(strarr[i]);
         }
 
