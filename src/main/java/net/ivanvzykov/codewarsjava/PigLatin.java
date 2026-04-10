5d57b7a326c8fa4d8b8c72a7a06897ace55a972e
Ivan Zykov <ivan.v.zykov@gmail.com>
2026-04-10 12:32:21 +0200

    Solve Simple Pig Latin

---
+++ src/main/java/net/ivanvzykov/codewarsjava/PigLatin.java
@@ -0,0 +1,8 @@
+package net.ivanvzykov.codewarsjava;
+
+// https://www.codewars.com/kata/520b9d2ad5c005041100000f
+public class PigLatin {
+    public static String pigIt(String str) {
+        return str.replaceAll("(\\w)(\\w*)", "$2$1ay");
+    }
+}
